4098da785fd8b791d0a5f3a6b1ac5145641affe3
Delia <Delia@Pcstudio>
2017-07-07 09:12:33 +0200

    final

--- src/it/polito/tdp/network/Main.java
+++ src/it/polito/tdp/network/Main.java
@@ -12,7 +12,6 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("Network.fxml"));
 			BorderPane root = (BorderPane) loader.load();
 			NetworkController controller = loader.getController();
